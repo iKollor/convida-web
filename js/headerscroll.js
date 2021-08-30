52d149c88a3f47a92f9b59f1ffb19306167d01d9
Kollor <isai_aulayvida@hotmail.com>
2021-08-30 18:13:18 -0500

    Update

--- js/headerscroll.js
+++ js/headerscroll.js
@@ -1,5 +1,11 @@
 var header = document.getElementById("navbar");
 var img = document.getElementById("logo");
+var tienda = document.getElementById("tienda");
+var tienda1 = document.getElementById("tienda1");
+var menu = document.getElementById("menu");
+var close = document.getElementById("close");
+var oscuro = document.getElementById("oscuro");
+var logonav = document.getElementById("logonav");
 
 window.addEventListener("scroll", function () {
   header.classList.toggle("sticky", window.scrollY > 0);
@@ -19,10 +25,26 @@ ischecked.onclick = function () {
     body.style.overflowY = "hidden";
     header.classList.remove("sticky");
     img.src = "../public/SVG/LOGO BLANCO.svg";
+    tienda.style.backgroundColor = "var(--morado)";
+    tienda1.style.display = "none";
+    menu.style.display = "none";
+    close.style.color = "var(--morado)";
+    close.style.display = "";
+    oscuro.style.display = "";
+    logonav.style.display = "";
+    if (window.scrollY > 200) {
+      img.style.display = "none";
+    }
   }
   if (ischecked.checked == false) {
     body.style.overflowY = "auto";
     header.classList.toggle("sticky", window.scrollY > 0);
+    img.style.display = "";
+    tienda1.style.display = "";
+    menu.style.display = "";
+    close.style.display = "none";
+    oscuro.style.display = "none";
+    logonav.style.display = "none";
     if (window.scrollY > 0) {
       img.src = "../public/SVG/logo-color.svg";
     }
