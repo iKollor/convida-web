14deb9d9198c63dbf50bb1bc827994da8eb4afd3
Kollor <isai_aulayvida@hotmail.com>
2021-11-05 18:20:33 -0500

    New scrollbar

--- js/header-footer.js
+++ js/header-footer.js
@@ -2,3 +2,10 @@ $(function () {
   $("#header").load("../../html/components/header.html");
   $("#footer").load("../../html/components/footer.html");
 });
+$(window).scroll(function () {
+  var scroll = $(window).scrollTop();
+  dh = $(document).height();
+  wh = $(window).height();
+  scrollPercent = (scroll / (dh - wh)) * 100;
+  $("#progressbar").css("height", scrollPercent + "%");
+});
